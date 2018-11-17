21c1285cf00c3b3bad4474e98028e170cd9bd452
Neil Sammut <neil.sammut@gmail.com>
2018-11-17 15:49:52 +0000

    sfgsdfds

--- 01_loader/01_loader.py
+++ 01_loader/01_loader.py
@@ -4,8 +4,7 @@ import requests
 
 def load_df_from_url(url="https://storage.googleapis.com/nshouseprice/train.csv"):
     """Loads a data frame from a URL. At the moment, the default set is the House Price dataset"""
-    url="https://storage.googleapis.com/nshouseprice/train.csv"
     s=requests.get(url).content
-    train=pd.read_csv(io.StringIO(s.decode('utf-8')))
-    return train
+    df_url = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    return df_url
 
